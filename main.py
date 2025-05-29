ec29e5259819ec495c8b0ee299cac4aa21bbc2d1
Giovane Nunes <giovane.nunes@silro.com.br>
2025-05-28 22:25:45 -0300

    update port

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def main() -> None:
     """Entrypoint to invoke when this module is invoked on the remote server."""
     # See the official documentations on how "0.0.0.0" makes the service available on
     # the local network - https://www.uvicorn.org/settings/#socket-binding
-    uvicorn.run("main:app", host="0.0.0.0", port=8005, log_level='info', reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level='info', reload=True)
 
 
 if __name__ == "__main__":
